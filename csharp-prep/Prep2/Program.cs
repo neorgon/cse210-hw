60b069533b5ac62c4a99c43b72e4cc3d6cfe87f5
neorgon <nestor.otondo@gmail.com>
2023-04-21 17:19:17 -0400

    Doing assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int percent = int.Parse(Console.ReadLine());
+
+        String grade = "";
+        String sign = "";
+
+        if (percent % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (percent % 10 < 3)
+        {
+            sign = "-";
+        }
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        }
+        else if (percent >= 80)
+        {
+            grade = "B" + sign;
+        }
+        else if (percent >= 70)
+        {
+            grade = "C" + sign;
+        }
+        else if (percent >= 60)
+        {
+            grade = "D" + sign;
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
     }
 }
