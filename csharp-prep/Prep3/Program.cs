e9b2f2ae489956d3cec7a6ffb55af0c6e877e903
Nestor Otondo <nestor.otondo@gmail.com>
2023-04-30 00:30:08 -0400

    usign while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
-using System;
 
+using System;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Is higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Is lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!");
+            }
+        }
     }
 }
