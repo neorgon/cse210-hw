99b8e99aeceb9a6078f7b42d5e2d7050e7bc5ff6
Nestor Otondo <nestor.otondo@gmail.com>
2023-05-19 21:34:58 -0400

    Create class Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fr1 = new Fraction(1, 1);
+        Console.WriteLine(fr1.GetFractionString());
+        Console.WriteLine(fr1.GetDecimalValue());
+        Fraction fr2 = new Fraction(5, 1);
+        Console.WriteLine(fr2.GetFractionString());
+        Console.WriteLine(fr2.GetDecimalValue());
+        Fraction fr3 = new Fraction(3, 4);
+        Console.WriteLine(fr3.GetFractionString());
+        Console.WriteLine(fr3.GetDecimalValue());
+        Fraction fr4 = new Fraction(1, 3);
+        Console.WriteLine(fr4.GetFractionString());
+        Console.WriteLine(fr4.GetDecimalValue());
     }
 }
