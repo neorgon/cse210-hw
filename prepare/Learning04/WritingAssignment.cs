a1df41cb4514fa2bfde7ec5709c745d7d1a68d0e
Nestor Otondo <nestor.otondo@gmail.com>
2023-06-03 19:47:40 -0400

    Adding constructor into WritingAssignment class

--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -1,9 +1,14 @@
-public class WritingAssignment
+public class WritingAssignment : Assignment
 {
     private string _title;
 
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
     public string getWrittingInformation()
     {
-        return "";
+        return $"{_title} by {_studentName}";
     }
 }
