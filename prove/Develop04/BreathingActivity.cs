1079fd8c86c1b3e1941a2b16f19a9d699275ef0f
Nestor Otondo <nestor.otondo@gmail.com>
2023-06-08 18:20:33 -0400

    Create Breathin Activity class

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,28 @@
+public class BreathingActivity : Activity
+{
+    private string _messageBreathingIn;
+    private string _messageBreathingOut;
+    private int _durationBreathingIn;
+    private int _durationBreathingOut;
+
+    public BreathingActivity(string wellcome, string ending, int duration, string description) : base(wellcome, ending, duration, description)
+    {
+    }
+
+    public void setMessages(string messageBreathingIn, string messageBreathinOut)
+    {
+        _messageBreathingIn = messageBreathingIn;
+        _messageBreathingOut = messageBreathinOut;
+    }
+
+    public void setTimers(int durationBreathingIn, int durationBreathingOut)
+    {
+        _durationBreathingIn = durationBreathingIn;
+        _durationBreathingOut = durationBreathingOut;
+    }
+
+    public override void run()
+    {
+        throw new NotImplementedException();
+    }
+}
