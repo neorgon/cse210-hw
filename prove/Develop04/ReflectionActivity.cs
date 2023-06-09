3b708174da1ba8e6058d94dbe3e7e657ed90e74b
Nestor Otondo <nestor.otondo@gmail.com>
2023-06-09 17:22:31 -0400

    Update Reflection activity class behaviors

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -2,7 +2,7 @@ public class ReflectionActivity : Activity
 {
     private List<string> _prompts;
     private List<string> _reflectQuestions;
-    private int durationBetweenReflections;
+    private int _durationBetweenReflections;
 
     public ReflectionActivity(string wellcome, string ending, int duration, string description) : base(wellcome, ending, duration, description)
     {
@@ -13,6 +13,17 @@ public class ReflectionActivity : Activity
     {
         _prompts = new List<string>();
         prompts.ForEach(prompt => _prompts.Add(prompt));
+    }
+
+    public void setReflectQuestions(List<string> questions)
+    {
+        _reflectQuestions = new List<string>();
+        questions.ForEach(question => _reflectQuestions.Add(question));
+    }
+
+    public void setDurationBetweenReflections(int duration)
+    {
+        _durationBetweenReflections = duration;
     }
 
     public override void run()
