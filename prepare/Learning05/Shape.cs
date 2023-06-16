c42ffa09f0f9fd58bcf16af69c129dbdb7010449
Nestor Otondo <nestor.otondo@gmail.com>
2023-06-15 23:53:03 -0400

    Create Shape class

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,14 @@
+public class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 1;
+    }
+}
