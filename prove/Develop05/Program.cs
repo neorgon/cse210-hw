c30bbd260ff53a2c138b8ce6b3e9c85baecef7cc
Nestor Otondo <nestor.otondo@gmail.com>
2023-06-27 15:36:08 -0400

    Adding some validations

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -27,22 +27,25 @@ class Program
         Console.WriteLine("\t 2. Eternal goal");
         Console.WriteLine("\t 3. Checklist goal");
         int goalType = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Choose a name for your goal: ");
-        string name = Console.ReadLine();
-        Console.Write("Choose a short description for your goal: ");
-        string description = Console.ReadLine();
-        Console.Write("Choose how many points you will earn if you accomplish this goal: ");
-        double points = Convert.ToDouble(Console.ReadLine());
-        int repeat = 0;
-        double bonus = 0;
-        if (goalType == 3)
+        if (goalType <= 3 || goalType >= 1)
         {
-            Console.Write("How many times would you repeat this goal? ");
-            repeat = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Choose how many points you will earn as a bonus: ");
-            bonus = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Choose a name for your goal: ");
+            string name = Console.ReadLine();
+            Console.Write("Choose a short description for your goal: ");
+            string description = Console.ReadLine();
+            Console.Write("Choose how many points you will earn if you accomplish this goal: ");
+            double points = Convert.ToDouble(Console.ReadLine());
+            int repeat = 0;
+            double bonus = 0;
+            if (goalType == 3)
+            {
+                Console.Write("How many times would you repeat this goal? ");
+                repeat = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Choose how many points you will earn as a bonus: ");
+                bonus = Convert.ToDouble(Console.ReadLine());
+            }
+            _goalTracker.CreateGoal(((Goals)goalType - 1), name, description, points, repeat, bonus);
         }
-        _goalTracker.CreateGoal(((Goals)goalType - 1), name, description, points, repeat, bonus);
     }
 
     private static void BackToMainMenu()
@@ -86,15 +89,23 @@ class Program
         Console.Clear();
         Console.WriteLine("[:] Record Event");
         Console.WriteLine("Choose a goal");
-        var goalList = _goalTracker.GetOnlyNotComplete();
         int index = 0;
-        foreach (var goal in _goalTracker.GetGoals())
+        var goals = _goalTracker.GetGoals();
+        foreach (var goal in goals)
         {
             index++;
             Console.WriteLine($"{index}: {(goal.IsComplete() ? "[X]" : "[ ]")} {goal.GetName()}");
         }
         index = Convert.ToInt32(Console.ReadLine());
-        _goalTracker.RecordEvent(index - 1);
+        if (index >= 1 || index <= goals.Count())
+        {
+            if (goals[index - 1].IsComplete())
+                Console.WriteLine("This goal is finished.");
+            else
+                _goalTracker.RecordEvent(index - 1);
+        }
+        else
+            Console.WriteLine("This goal do not exist.");
         BackToMainMenu();
     }
 
